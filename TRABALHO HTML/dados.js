986acc067dc11d5e03b14a6a11addfb179243523
GuilhermeC11 <guilhermefer11@hotmail.com>
2025-05-18 16:24:04 -0300

    Primeiro upload do site

---
+++ TRABALHO HTML/dados.js
@@ -0,0 +1,20 @@
+let dados = [
+    {
+        titulo : 'Rosa',
+        descricao : 'A rosa é uma das flores mais populares e apreciadas em todo o mundo. Conhecida por sua beleza delicada e perfume suave, ela simboliza o amor, a paixão e a beleza feminina. As rosas vêm em uma variedade de cores e formas, cada uma com seu próprio significado',
+        link : 'https://pt.wikipedia.org/wiki/Rosa',
+        tags : 'rosa, apreciadas'
+    },
+    {
+        titulo : 'Rosa do Deserto',
+        descricao : 'A rosa do deserto, apesar do nome, não é uma rosa verdadeira. Nativa de regiões áridas da África e da Península Arábica, ela é conhecida por sua resistência e pelas flores vibrantes em diversas tonalidades. É uma planta suculenta que armazena água em seu caule, adaptando-se a climas secos.',
+        link : 'https://www.jardineiro.net/plantas/rosa-do-deserto',
+        tags : 'rosa, deserto'
+      },
+      {
+        titulo : 'Orquídea',
+        descricao : 'As orquídeas são uma das famílias de plantas mais diversas e fascinantes do mundo. Conhecidas por suas flores exóticas e elegantes, elas são apreciadas por colecionadores e amantes de plantas por sua beleza e complexidade. Existem milhares de espécies de orquídeas, cada uma com características únicas.',
+        link : 'https://www.jardineiro.net/plantas/orquidea',
+        tags : 'diversas, dascinantes'
+      }
+];
